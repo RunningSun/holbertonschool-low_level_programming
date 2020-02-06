72ea2046db21a157f0ead677eb27ff7a327ad8ad
RunningSun <joycejql@gmail.com>
2020-02-06 19:55:01 +0000

    add file8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+	char c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
