741f951249f235c15f91cb8fdf2ec72bce140497
RunningSun <joycejql@gmail.com>
2020-02-07 00:17:34 +0000

    update file9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-		if (n < '9')
+		putchar(n + '0');
+		if (n < 9)
 		{
 		putchar(',');
 		putchar(' ');
