0a0152f44801c47ae49f962feb878bd910704cba
RunningSun <joycejql@gmail.com>
2020-02-08 19:48:43 +0000

    add file100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 99; i++)
+	{
+		if ((i / 10) < (i % 10))
+			{
+			putchar ('0' + i / 10);
+			putchar ('0' + i % 10);
+		if (i < 99)
+			{
+			putchar (',');
+			putchar (' ');
+			}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
