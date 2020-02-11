3606cb4e36ac7b6508264d5225b8145dabffe9b6
RunningSun <joycejql@gmail.com>
2020-02-11 11:29:19 +0000

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
-  * Description: Prints the alphabet a to z 10 times
+  * print_alphabet_x10 - print the alphabet 10 times
   *
+  * Description: Uses header to link to another function
+  * Retrun: void
   */
 
 void print_alphabet_x10(void)
 	{
 	char c;
 	int i = 0;
+
 	while (i <= 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
