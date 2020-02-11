14b891fe99d1e4b51d6e3c9744db2fc150d69fae
RunningSun <joycejql@gmail.com>
2020-02-11 11:12:59 +0000

    print holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+putchar (72);
+putchar (111);
+putchar (108);
+putchar (98);
+putchar (101);
+putchar (114);
+putchar (116);
+putchar (111);
+putchar (110);
+putchar ('\n');
+return (0);
+}
