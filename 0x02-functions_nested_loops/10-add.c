2bc4cc849799b659c03b00f01b13494cddd46422
RunningSun <joycejql@gmail.com>
2020-02-11 11:48:47 +0000

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * add - adds two integers and return the result
+  * @x: An int from main
+  * @y: An int from main
+  * Return: 0 when done
+  */
+
+int add(int x, int y)
+	{
+		return (x + y);
+	}
