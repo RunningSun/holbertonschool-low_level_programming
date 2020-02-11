892e6134a172787980b981c501d45a4e1c1be379
RunningSun <joycejql@gmail.com>
2020-02-11 11:21:58 +0000

    update print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+  * print_alphabet - prints out the alphabet with a new line
+  * Description: Uses headers to link to another function
+  * Return: 0 when well done
+  */
+
 void print_alphabet(void)
 {
 	char c;
