fe7044e92aae474b7c8b47ea944d86b642b16974
RunningSun <joycejql@gmail.com>
2020-02-13 17:47:00 +0000

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c: an int passed by main function
+ *
+ * Description: Uses headers to link function
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	return (1);
+return (0);
+}
