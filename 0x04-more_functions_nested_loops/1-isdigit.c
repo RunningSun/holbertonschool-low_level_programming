1542fb14c71ec3edf7298d6d8b594d501fb20cff
RunningSun <joycejql@gmail.com>
2020-02-13 17:49:35 +0000

    checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check for digit 0 to 9
+ * @c: an int passed by main function
+ *
+ * Description: Uses headers to link function
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+	return (1);
+return (0);
+}
