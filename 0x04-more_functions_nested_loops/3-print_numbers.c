c2a423c11c6c14aa39cfa6288566d26363cbef4b
RunningSun <joycejql@gmail.com>
2020-02-13 18:03:53 +0000

    prints the numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,13 @@
  * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	c = 0;
+	int c;
 
 	for (c = 0; c <= 9; c++)
-		_putchar(c + '0');
+	{
+	_putchar(c + '0');
+	}
 	_putchar('\n');
 }
